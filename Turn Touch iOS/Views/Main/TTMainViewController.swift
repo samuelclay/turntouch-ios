fa90e2034bbf6409b6b4ca11b5b09db71e60ed4c
Samuel Clay <samuel@ofbrooklyn.com>
2016-05-25 14:51:36 -0700

    Title constraints for size classes

--- Turn Touch iOS/Views/Main/TTMainViewController.swift
+++ Turn Touch iOS/Views/Main/TTMainViewController.swift
@@ -14,13 +14,15 @@ class TTMainViewController: UIViewController {
     var titleBarView: TTTitleBarView = TTTitleBarView()
     var modeTabsView: UIStackView!
     var modeTabs: [TTModeTab] = []
+    var titleBarConstraint = NSLayoutConstraint()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         stackView.addArrangedSubview(titleBarView)
         stackView.addConstraint(NSLayoutConstraint(item: titleBarView, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: stackView, attribute: NSLayoutAttribute.Width, multiplier: 1.0, constant: 0.0))
-        stackView.addConstraint(NSLayoutConstraint(item: titleBarView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: 40))
+        titleBarConstraint = NSLayoutConstraint(item: titleBarView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: 44)
+        stackView.addConstraint(titleBarConstraint)
         
         modeTabs = [
             TTModeTab(modeDirection:.NORTH),
@@ -44,6 +46,14 @@ class TTMainViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
+    override func traitCollectionDidChange(previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        if self.view.traitCollection.verticalSizeClass == UIUserInterfaceSizeClass.Compact {
+            titleBarConstraint.constant = 28;
+        } else {
+            titleBarConstraint.constant = 44;
+        }
+    }
 
     /*
     // MARK: - Navigation
