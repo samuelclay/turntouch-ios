efe6cdc8e41270ea9962e7eb40e93384870f9237
Samuel Clay <samuel@ofbrooklyn.com>
2016-06-14 15:42:56 -0700

    Adding Phillips Hue SDK.

---
+++ Turn Touch iOS/Vendor/Turn_Touch_iOS-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  HueBridgingHeader.h
+//  Turn Touch iOS
+//
+//  Created by Samuel Clay on 6/14/16.
+//  Copyright © 2016 Turn Touch. All rights reserved.
+//
+
+#ifndef HueBridgingHeader_h
+#define HueBridgingHeader_h
+
+#import <HueSDK_iOS/HueSDK.h>
+
+#endif /* HueBridgingHeader_h */
