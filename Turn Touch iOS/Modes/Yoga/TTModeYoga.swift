43328a5926d8dda81a449ffbf47ff3f1e3083ec3
Samuel Clay <samuel@ofbrooklyn.com>
2016-09-20 18:26:45 -0700

    Adding in yoga positions, attempting to draw positions. Something's off with the scaling.

--- Turn Touch iOS/Modes/Yoga/TTModeYoga.swift
+++ Turn Touch iOS/Modes/Yoga/TTModeYoga.swift
@@ -10,18 +10,91 @@ import UIKit
 
 class TTModeYoga: TTMode {
 
-    var yogaActive = false
-    var yogaViewController = TTModeYogaViewController()
+    static var yogaActive = false
+    var yogaViewController: TTModeYogaViewController!
     var yogaNavController: UINavigationController!
 //    var yogaState: TTModeCameraState = .cameraInactive
     var yogaPosition = 0
     let MAX_YOGA_POSITIONS = 20
     
+    let poses = [
+        ["name": "Extended triangle pose",
+         "file": "yoga_position_1",
+         "lang": "Utthita Trikonasana",
+         "desc": "A standing yoga pose that tones the legs, reduces stress, and increases stability.",
+         "from": "The word \"Trikonasana\" comes from the Sanskrit words \"tri\", meaning \"three\", \"kona\", meaning \"angle\", and \"asana\", meaning \"pose\"."],
+        ["name": "Tree pose",
+         "file": "yoga_position_2",
+         "lang": "Vriksasana",
+         "desc": "The asana emphasizes alignment of the head, spine and hips.",
+         "from": "The name comes from the Sanskrit words vriksa, meaning \"tree\", and asana, meaning \"pose\"."],
+        ["name": "Warrioir I pose",
+         "file": "yoga_position_3",
+         "lang": "Virabhadrasana I",
+         "desc": ""],
+        ["name": "Cat pose",
+         "file": "yoga_position_4",
+         "lang": "Marjariasana",
+         "desc": ""],
+        ["name": "Boat pose",
+         "file": "yoga_position_5",
+         "lang": "Navasana",
+         "desc": ""],
+        ["name": "Low lunge pose",
+         "file": "yoga_position_6",
+         "lang": "Ashva Sanchalanasana",
+         "desc": ""],
+        ["name": "Warrior II pose",
+         "file": "yoga_position_7",
+         "lang": "Virabhadrasana II",
+         "desc": ""],
+        ["name": "Bow pose",
+         "file": "yoga_position_8",
+         "lang": "Dhanurasana",
+         "desc": ""],
+        ["name": "Plow pose",
+         "file": "yoga_position_9",
+         "lang": "Halasana",
+         "desc": ""],
+        ["name": "Cobra pose",
+         "file": "yoga_position_10",
+         "lang": "Bhujangasana",
+         "desc": ""],
+        ["name": "Downward-facing dog pose",
+         "file": "yoga_position_11",
+         "lang": "Adho Mukha Shvanasana",
+         "desc": ""],
+        ["name": "Chair pose",
+         "file": "yoga_position_12",
+         "lang": "Utkatasana",
+         "desc": ""],
+        ["name": "Child's pose",
+         "file": "yoga_position_13",
+         "lang": "Balasana",
+         "desc": ""],
+        ["name": "Vajra pose",
+         "file": "yoga_position_14",
+         "lang": "Vajrasana",
+         "desc": ""],
+        ["name": "Seated forward bend",
+         "file": "yoga_position_15",
+         "lang": "Paschimottanasana",
+         "desc": ""],
+        ["name": "Uttanasana pose with clasped hands",
+         "file": "yoga_position_16",
+         "lang": "Uttanasana",
+         "desc": ""],
+    ]
+    
     required init() {
         super.init()
         
-        yogaNavController = UINavigationController(rootViewController: yogaViewController)
-        yogaNavController.modalPresentationStyle = .fullScreen
+//        yogaNavController = UINavigationController(rootViewController: yogaViewController)
+//        yogaNavController.modalPresentationStyle = .fullScreen
+        yogaViewController = TTModeYogaViewController(nibName: "TTModeYogaViewController", bundle: nil)
+        yogaViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        yogaViewController.modalTransitionStyle = .crossDissolve
+        yogaViewController.modalPresentationStyle = .fullScreen
         yogaViewController.modeYoga = self
     }
     
@@ -109,22 +182,22 @@ class TTModeYoga: TTMode {
     
     override func deactivate() {
         self.closeYoga()
-        yogaActive = false
+        TTModeYoga.yogaActive = false
     }
     
     func closeYoga() {
         appDelegate().mainViewController.dismiss(animated: true) {
-            self.yogaActive = false
+            TTModeYoga.yogaActive = false
         }
     }
     
     func ensureYoga() -> Bool {
         var yogaAlreadyShowing = true
         
-        if !self.yogaActive {
+        if !TTModeYoga.yogaActive {
             yogaAlreadyShowing = false
-            self.yogaActive = true
-            appDelegate().mainViewController.present(yogaNavController,
+            TTModeYoga.yogaActive = true
+            appDelegate().mainViewController.present(yogaViewController,
                                                      animated: true) {
             }
         }
--- Turn Touch iOS/Modes/Yoga/TTModeYogaViewController.swift
+++ Turn Touch iOS/Modes/Yoga/TTModeYogaViewController.swift
@@ -12,53 +12,20 @@ class TTModeYogaViewController: UIViewController {
 
     var modeYoga: TTModeYoga!
     var backgroundImageView: UIImageView!
+    @IBOutlet var yogaPoseImage: UIImageView!
+    @IBOutlet var yogaName: UILabel!
+    @IBOutlet var yogaLang: UILabel!
     
-    override func viewDidAppear(_ animated: Bool) {
-        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    override func viewWillAppear(_ animated: Bool) {
+//        self.navigationController?.setNavigationBarHidden(true, animated: false)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.translatesAutoresizingMaskIntoConstraints = false
+//        self.view.translatesAutoresizingMaskIntoConstraints = false
         self.view.backgroundColor = UIColor.blue
-        
-        let blurEffect = UIBlurEffect(style: .light)
-        let visualEffectView = UIVisualEffectView(effect: blurEffect)
-        let image = UIImage(named: "yoga_background.jpg")
-        backgroundImageView = UIImageView(image: image)
-        backgroundImageView.contentMode = .scaleAspectFill
-        backgroundImageView.clipsToBounds = true
-        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
-        visualEffectView.translatesAutoresizingMaskIntoConstraints = false
-
-        self.view.addSubview(backgroundImageView)
-        self.view.addConstraint(NSLayoutConstraint(item: backgroundImageView, attribute: .width,
-                                                   relatedBy: .equal, toItem: self.view, attribute: .width,
-                                                   multiplier: 1.0, constant: 0))
-        self.view.addConstraint(NSLayoutConstraint(item: backgroundImageView, attribute: .height,
-                                                   relatedBy: .equal, toItem: self.view, attribute: .height,
-                                                   multiplier: 1.0, constant: 0))
-        self.view.addConstraint(NSLayoutConstraint(item: backgroundImageView, attribute: .top,
-                                                   relatedBy: .equal, toItem: self.view, attribute: .top,
-                                                   multiplier: 1.0, constant: 0))
-        self.view.addConstraint(NSLayoutConstraint(item: backgroundImageView, attribute: .left,
-                                                   relatedBy: .equal, toItem: self.view, attribute: .left,
-                                                   multiplier: 1.0, constant: 0))
-
-//        backgroundImageView.addSubview(visualEffectView)
-//        backgroundImageView.addConstraint(NSLayoutConstraint(item: visualEffectView, attribute: .width,
-//                                                   relatedBy: .equal, toItem: backgroundImageView, attribute: .width,
-//                                                   multiplier: 1.0, constant: 0))
-//        backgroundImageView.addConstraint(NSLayoutConstraint(item: visualEffectView, attribute: .height,
-//                                                   relatedBy: .equal, toItem: backgroundImageView, attribute: .height,
-//                                                   multiplier: 1.0, constant: 0))
-//        backgroundImageView.addConstraint(NSLayoutConstraint(item: visualEffectView, attribute: .top,
-//                                                   relatedBy: .equal, toItem: backgroundImageView, attribute: .top,
-//                                                   multiplier: 1.0, constant: 0))
-//        backgroundImageView.addConstraint(NSLayoutConstraint(item: visualEffectView, attribute: .left,
-//                                                   relatedBy: .equal, toItem: backgroundImageView, attribute: .left,
-//                                                   multiplier: 1.0, constant: 0))
-}
+//        self.view.clipsToBounds = true
+    }
 
     override var prefersStatusBarHidden : Bool {
         return true
